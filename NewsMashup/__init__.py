ce736def03c3488ea2b97e9df068f330354212d3
Chris James Martin <cjmartin@gmail.com>
2011-09-12 22:46:15 -0300

    fetch a simple twitter search and dump the results

--- NewsMashup/__init__.py
+++ NewsMashup/__init__.py
@@ -3,10 +3,17 @@ from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.api import taskqueue
 from google.appengine.ext import db
 from google.appengine.ext.webapp import template
+from django.utils import simplejson
+
+import urllib,urllib2
 
 class MainApp(webapp.RequestHandler):
     
     def get(self) :
         
+        request = "http://search.twitter.com/search.json?q=cjmartin&result_type=mixed&count=5"
+        res = urllib2.urlopen(request)
+        json = simplejson.loads(res.read())
+        
         self.response.headers['Content-Type'] = 'text/plain'
-        self.response.out.write('Hello, webapp World!')
+        self.response.out.write(json)
