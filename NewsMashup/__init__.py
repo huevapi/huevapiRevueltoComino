ab85deb6dbe0b30757c0120e38d0b8d0cedd2a85
Chris James Martin <cjmartin@gmail.com>
2011-09-12 23:31:40 -0300

    twitter query and dump tweets

--- NewsMashup/__init__.py
+++ NewsMashup/__init__.py
@@ -6,14 +6,34 @@ from google.appengine.ext.webapp import template
 from django.utils import simplejson
 
 import urllib,urllib2
+import re
 
 class MainApp(webapp.RequestHandler):
     
     def get(self) :
         
-        request = "http://search.twitter.com/search.json?q=cjmartin&result_type=mixed&count=5"
-        res = urllib2.urlopen(request)
-        json = simplejson.loads(res.read())
+        sites = [
+        'emol.com',
+        'latercera.com',
+        'cooperativa.cl',
+        'nacion.cl',
+        'elmostrador.cl',
+        'terra.cl',
+        ]
         
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.out.write(json)
+        for site in sites :
+            # enc_site = urllib.urlencode(site)
+            self.response.out.write("Querying for %s <br />" % site)
+            request = "http://search.twitter.com/search.json?q=" + site + "&result_type=mixed&count=5"
+            res = urllib2.urlopen(request)
+            json = simplejson.loads(res.read())
+            tweets = json['results']
+            
+            for tweet in tweets :
+                #matches = re.search("\b(([\w-]+://?|www[.])[^\s()<>]+(?:\([\w\d]+\)|([^[:punct:]\s]|/)))", tweet)
+                
+                #for match in matches :
+                #    self.response.out.write(match)
+                    
+                self.response.out.write(tweet['text'])
+                self.response.out.write("<br /><be />")
